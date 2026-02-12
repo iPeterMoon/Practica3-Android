985f5940f362e6dc15f77019c3e47cf16715b854
Pedro Luna <106363641+iPeterMoon@users.noreply.github.com>
2026-02-11 20:20:22 -0700

    UI y navegacion

---
+++ Practica3/app/src/main/java/mx/itson/edu/practica3/RegisterActivity.kt
@@ -0,0 +1,29 @@
+package mx.itson.edu.practica3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button: Button = findViewById<Button>(R.id.btn_sign_in)
+
+        button.setOnClickListener {
+            val intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
